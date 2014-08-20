c9fc6778d28fd6bf5ed96ea23ced45541abd2e56
zimme <zimme@users.noreply.github.com>
2014-08-20 20:18:25 +0200

    Version bump
    Change version to match select2-boostrap-css

--- package.js
+++ package.js
@@ -1,5 +1,6 @@
 Package.describe({
-  summary: "CSS to make Select2 fit in with Bootstrap 3"
+  summary: "CSS to make Select2 fit in with Bootstrap 3",
+  version: "1.4.1"
 });
 
 Package.on_use(function (api, where) {
