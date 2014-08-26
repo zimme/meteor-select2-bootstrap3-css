fb4e93bcad9309d05d0b4347feef5d2bea9f52f7
zimme <zimme@users.noreply.github.com>
2014-08-26 23:25:08 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,8 @@
 Package.describe({
   summary: 'CSS to make Select2 fit in with Bootstrap 3',
-  version: '0.0.2'
+  name: 'zimme:select2-bootstrap3-css',
+  git: 'https://github.com/zimme/meteor-select2-bootstrap3-css.git',
+  version: '0.0.4'
 });
 
 Package.on_use(function (api, where) {
