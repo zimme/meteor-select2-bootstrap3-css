50b64f876b7a691996aa36691470c2a9856af130
zimme <zimme@users.noreply.github.com>
2014-09-11 15:18:19 +0200

    Remove dependencies
    Users should be able to use which packages the want for bootstrap3 and
    select2

--- package.js
+++ package.js
@@ -8,10 +8,5 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('0.9.0');
 
-  api.use([
-    'mizzao:bootstrap-3@3.2.0',
-    'natestrauser:select2@3.4.7'
-  ], 'client');
-
   api.addFiles('select2-bootstrap-css/select2-bootstrap.css', 'client');
 });
