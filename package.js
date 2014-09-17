d33ef5ac8bdefb38759711f73d09e59161ff8eca
zimme <zimme@users.noreply.github.com>
2014-09-17 15:38:40 +0200

    Add weak dependencies
    Add the most popular bootstrap3 and selec2 packages as weak
    dependencies to try and make sure they are loaded before this package

--- package.js
+++ package.js
@@ -8,5 +8,11 @@ Package.describe({
 Package.onUse(function (api) {
   api.versionsFrom('0.9.0');
 
+  api.use([
+    'mizzao:bootstrap-3@3.2.0',
+    'natestrauser:select2@3.4.7',
+    'simison:bootstrap3-less@0.3.0'
+  ], ['client', 'server'], {weak: true});
+
   api.addFiles('select2-bootstrap-css/select2-bootstrap.css', 'client');
 });
